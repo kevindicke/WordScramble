920781972844d0c537b3f40d9e1d42eb17455f04
Kevin Dicke <k.dicke7@gmail.com>
2019-11-02 17:09:30 -0700

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Kevin Dicke on 11/2/19.
+//  Copyright © 2019 Spritztour. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
